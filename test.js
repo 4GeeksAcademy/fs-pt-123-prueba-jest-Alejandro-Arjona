0a7b987f38bce9be8feb16dfbd882d6bdad077df
Alejandro Arjona López <alex.arjona@hotmail.com>
2025-10-24 19:05:24 +0000

    Pruebas unitarias realizadas

---
+++ test.js
@@ -0,0 +1,27 @@
+const { sum } = require('./app.js');
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14, 9);
+    expect(total).toBe(23);
+});
+
+test("One euro should be 1.07 dollars", function () {
+    const { fromEuroToDollar } = require('./app.js');
+    const dollars = fromEuroToDollar(3.5);
+    const expected = 3.5 * 1.07;
+    expect(fromEuroToDollar(3.5)).toBe(3.745);
+})
+
+test("One dollar should be approximately 146.26 yenes", function () {
+    const { fromDollarToYen } = require('./app.js');
+    const yenes = fromDollarToYen(1);
+    const expected = (1 / 1.07) * 156.5;
+    expect(yenes).toBeCloseTo(expected, 2); // permite margen de error
+});
+
+test("One yen should be approximately 0.00578 libras", function () {
+    const { fromYenToPound } = require('./app.js');
+    const pounds = fromYenToPound(1);
+    const expected = (1 / 156.5) * 0.87;
+    expect(pounds).toBeCloseTo(expected, 5); // más precisión porque es un número pequeño
+});
+
