34bf58fd61425328b790a6a440aa61ad33730117
Alejandro Arjona López <alex.arjona@hotmail.com>
2025-10-24 19:15:39 +0000

    Cambios y correcciones

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-let oneEuroIs = {
+let valorEuro = {
     "JPY": 156.5,
     "USD": 1.07,
     "GBP": 0.87
 };
 
-const fromEuroToDollar = function (valueInEuro) {
-    let valueInDollar = valueInEuro * 1.07;
-    return valueInDollar;
+const fromEuroToDollar = function (valorEnEuro) {
+    let valorEnDolares = valorEnEuro * 1.07;
+    return valorEnDolares;
 }
 
-const fromDollarToYen = function (valueInDollar) {
-    let valueInEuro = valueInDollar / oneEuroIs.USD;
-    let valueInYen = valueInEuro * oneEuroIs.JPY;
-    return valueInYen;
+const fromDollarToYen = function (valorEnDolares) {
+    let valorEnEuro = valorEnDolares / valorEuro.USD;
+    let valorEnYen = valorEnEuro * valorEuro.JPY;
+    return valorEnYen;
 };
 
-const fromYenToPound = function (valueInYen) {
-    let valueInEuro = valueInYen / oneEuroIs.JPY;
-    let valueInPound = valueInEuro * oneEuroIs.GBP;
-    return valueInPound;
+const fromYenToPound = function (valorEnYen) {
+    let valorEnEuro = valorEnYen / valorEuro.JPY;
+    let valorEnLibras = valorEnEuro * valorEuro.GBP;
+    return valorEnLibras;
 };
 
 console.log("Hello World")
--- test.js
+++ test.js
@@ -4,9 +4,9 @@ test('adds 14 + 9 to equal 23', () => {
     expect(total).toBe(23);
 });
 
-test("One euro should be 1.07 dollars", function () {
+test("One euro should be 1.07 dolares", function () {
     const { fromEuroToDollar } = require('./app.js');
-    const dollars = fromEuroToDollar(3.5);
+    const dolares = fromEuroToDollar(3.5);
     const expected = 3.5 * 1.07;
     expect(fromEuroToDollar(3.5)).toBe(3.745);
 })
@@ -15,13 +15,13 @@ test("One dollar should be approximately 146.26 yenes", function () {
     const { fromDollarToYen } = require('./app.js');
     const yenes = fromDollarToYen(1);
     const expected = (1 / 1.07) * 156.5;
-    expect(yenes).toBeCloseTo(expected, 2); // permite margen de error
+    expect(yenes).toBeCloseTo(expected, 2);
 });
 
 test("One yen should be approximately 0.00578 libras", function () {
     const { fromYenToPound } = require('./app.js');
-    const pounds = fromYenToPound(1);
+    const libras = fromYenToPound(1);
     const expected = (1 / 156.5) * 0.87;
-    expect(pounds).toBeCloseTo(expected, 5); // más precisión porque es un número pequeño
+    expect(pounds).toBeCloseTo(expected, 5);
 });
 
